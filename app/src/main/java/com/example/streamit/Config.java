4437a49fb38de76851f970998bad33cf1a58b072
Vipul <Vipulsuthar135@gmail.com>
2020-07-30 13:20:16 +0530

    Retrofit Client Created

---
+++ app/src/main/java/com/example/streamit/Config.java
@@ -0,0 +1,12 @@
+package com.example.streamit;
+
+public class Config {
+
+    public static final String API_SERVER_URL = "http://demo.redtvlive.com/oxoo/v12/rest-api/";
+
+    //copy your api key from php admin dashboard & paste below
+    public static final String API_KEY = "oyktaah8kjh8y6inf81rnn8g";
+
+    //copy your terms url from php admin dashboard & paste below
+    public static final String TERMS_URL = "https://spagreen.net/tearms-of-services/";
+}
